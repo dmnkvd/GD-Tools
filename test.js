dbc9c86a2abfb7c0f991d1f28a2b407fadaba82a
dmnkvd <dominikvr@gmail.com>
2020-04-10 10:20:05 +0200

    loop modifications

---
+++ test.js
@@ -0,0 +1,46 @@
+selection = [
+    type = [
+        {
+          'Publication':'A printed work produced as a serial.'
+          },
+        {
+          'Poster':'A single printed work, containing a statement.'
+          },
+        {
+          'Zine':'An informal, typically photographic entry.'
+          },
+        {
+          'Book':'An informal, typically photographic entry.'
+          },
+        {
+          'Brochure':'An informal, typically photographic entry.'
+          },
+    ],
+
+    format = [
+        'A4',
+        'Letter',
+        'A3',
+        'A2',
+        'A5',
+        'B6'
+    ],
+
+    focus = [
+        'Contrast',
+        'Letter Spacing',
+        'Whitespace',
+        'Kerning',
+    ],
+
+    print = [
+        'Inkjet',
+        'Risoprint',
+        'Silkscreen',
+        'Sublimation Print',
+        'UV Print',
+        'Xerox',
+    ],
+        ]
+      
+    console.log(selection[1])
